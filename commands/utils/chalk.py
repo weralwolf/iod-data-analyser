b9f44ccb4da1b1d981b55efe62178a8c27324632
Anatolii Koval <weralwolf@gmail.com>
2018-06-19 23:54:47 +0300

    Logger collored output

--- commands/utils/chalk.py
+++ commands/utils/chalk.py
@@ -7,11 +7,16 @@ def _make_color_function(color):
     return wrapped
 
 
-noticeRed = _make_color_function(fg(256) + bg(9) + attr('bold'))
-noticeGreen = _make_color_function(fg(256) + bg(76) + attr('bold'))
-noticeYellow = _make_color_function(fg(256) + bg(208) + attr('bold'))
+noticeRed = _make_color_function(fg(255) + bg(9) + attr('bold'))
+noticeGreen = _make_color_function(fg(255) + bg(76) + attr('bold'))
+noticeYellow = _make_color_function(fg(255) + bg(208) + attr('bold'))
 
 
-red = _make_color_function(fg(9) + bg(0) + attr('bold'))
-green = _make_color_function(fg(76) + bg(0) + attr('bold'))
-yellow = _make_color_function(fg(208) + bg(0) + attr('bold'))
+red = _make_color_function(fg(9) + bg(0))
+red_bold = _make_color_function(fg(9) + bg(0) + attr('bold'))
+green = _make_color_function(fg(28) + bg(0))
+green_bold = _make_color_function(fg(76) + bg(0) + attr('bold'))
+yellow = _make_color_function(fg(208) + bg(0))
+yellow_bold = _make_color_function(fg(208) + bg(0) + attr('bold'))
+dim_grey = _make_color_function(fg(241) + bg(0) + attr('dim'))
+dim_light_grey = _make_color_function(fg(251) + bg(0) + attr('dim'))
--- commands/utils/logger.py
+++ commands/utils/logger.py
@@ -5,8 +5,11 @@ from logging.handlers import TimedRotatingFileHandler
 
 from ionospheredata.settings import LOGS_DIR, LOGGING_LEVEL
 
+from .chalk import green, yellow, dim_grey, dim_light_grey
+
 logger = getLogger('iod')
 logger.setLevel(LOGGING_LEVEL or DEBUG)
+
 
 file_handler = TimedRotatingFileHandler(join(LOGS_DIR, 'iod.log'), when='H', interval=1, utc=True)
 file_handler.setLevel(LOGGING_LEVEL or INFO)
@@ -16,6 +19,12 @@ logger.addHandler(file_handler)
 
 stdout_handler = StreamHandler(stream=stdout)
 stdout_handler.setLevel(LOGGING_LEVEL or INFO)
-stdout_formatter = Formatter('%(asctime)s::%(name)s:/%(filename)s # %(funcName)s ~ %(levelname)s# %(message)s')
+stdout_formatter = Formatter(
+    dim_grey('%(asctime)s::%(name)s:/') +
+    dim_light_grey('%(filename)s # %(funcName)s ~ ') +
+    yellow('%(levelname)s') +
+    dim_grey(' # ') +
+    green('%(message)s')
+)
 stdout_handler.setFormatter(stdout_formatter)
 logger.addHandler(stdout_handler)
