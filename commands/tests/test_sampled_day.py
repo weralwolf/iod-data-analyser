ba24e9185f650cfbe7522dc7b28cc62e14cc0574
Anatolii Koval <weralwolf@gmail.com>
2018-07-25 19:37:27 +0300

    Sampled day
    [Description]
    - Compute sampled data over day of the year;
    - Create a window view on parsed data;

--- commands/tests/test_sampled_day.py
+++ commands/tests/test_sampled_day.py
@@ -7,7 +7,7 @@ from commands.artifacts.sampled_day import make_deltas, sampled_day
 from numpy import all
 
 
-def sampled_dat_continuity_params(source_marker: str, year: int, day: int, sampling: int, *continuity_params: List[str]):
+def sampled_day_continuity_params(source_marker: str, year: int, day: int, sampling: int, *continuity_params: List[str]):
     for chunk in sampled_day(source_marker, year, day, sampling, *continuity_params):
         ut = chunk.get('ut', transposed=True)[0] / 1000
         deltas = make_deltas(ut)
@@ -16,20 +16,21 @@ def sampled_dat_continuity_params(source_marker: str, year: int, day: int, sampl
 
 
 def test_sampled_day_continuity_params_1():
+    for year, day in list(NACS_TEST_FILES_OF_THE_DAY.keys()):
+        sampled_day_continuity_params(DE2_SOURCE_NACS, year, day, 1)
+
+
+def test_sampled_day_continuity_params_2():
     year, day = list(NACS_TEST_FILES_OF_THE_DAY.keys())[0]
-    sampled_dat_continuity_params(DE2_SOURCE_NACS, year, day, 1)
+    sampled_day_continuity_params(DE2_SOURCE_NACS, year, day, 2)
 
 
-def test_sampled_dat_continuity_params_2():
+def test_sampled_day_continuity_params_3():
     year, day = list(NACS_TEST_FILES_OF_THE_DAY.keys())[0]
-    sampled_dat_continuity_params(DE2_SOURCE_NACS, year, day, 2)
+    sampled_day_continuity_params(DE2_SOURCE_NACS, year, day, 3)
 
 
-def test_sampled_dat_continuity_params_3():
+def test_sampled_day_continuity_params_4():
     year, day = list(NACS_TEST_FILES_OF_THE_DAY.keys())[0]
-    sampled_dat_continuity_params(DE2_SOURCE_NACS, year, day, 3)
+    sampled_day_continuity_params(DE2_SOURCE_NACS, year, day, 4)
 
-
-def test_sampled_dat_continuity_params_4():
-    year, day = list(NACS_TEST_FILES_OF_THE_DAY.keys())[0]
-    sampled_dat_continuity_params(DE2_SOURCE_NACS, year, day, 4)
