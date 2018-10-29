dac48e97e0db210b867817fbd70f52a489e840e9
Anatolii Koval <weralwolf@gmail.com>
2018-10-29 21:39:00 +0100

    WP wave decomposition

--- commands/artifacts/__init__.py
+++ commands/artifacts/__init__.py
@@ -1,6 +1,7 @@
-from .all_files import all_files  # noqa: F401
-from .list_days import list_days  # noqa: F401
-from .parsed_data import parsed_data  # noqa: F401
-from .sampled_day import sampled_day  # noqa: F401
+from .all_files import all_files  # noqa:E401
+from .list_days import list_days  # noqa:E401
+from .parsed_data import parsed_data  # noqa:E401
+from .sampled_day import sampled_day  # noqa:E401
 from .moving_average import moving_average  # noqa:E401
-from .base_filtering.not_monotone_files import not_monotone_files  # noqa: F401
+from .wave_decomposition import wave_decomposition  # noqa:E401
+from .base_filtering.not_monotone_files import not_monotone_files  # noqa:E401
---
+++ commands/tests/test_wave_decomposition.py
@@ -0,0 +1,25 @@
+from os.path import join
+from commands.parsers import FileParser
+from commands.artifacts import wave_decomposition
+from commands.artifacts.moving_average import moving_average_sequence
+from commands.parsers.de2 import SourceNACSRow
+from commands.settings.de2 import DE2_SOURCE_NACS
+from commands.utils.logger import logger
+from commands.utils.resolve_data_source import resolve_data_source
+
+from numpy import abs, sum, max
+
+
+def test_wave_decomposition() -> None:
+    path, _, _, _ = resolve_data_source(DE2_SOURCE_NACS)
+    parser = FileParser(SourceNACSRow, join(path, '1981295T072140_0_DE2_NACS_1S_V01.ASC'))
+    parts = wave_decomposition(parser, 'o_dens')
+    value = parser.get('o_dens', transposed=True)[0]
+    avg = moving_average_sequence(value)
+    logger.error(avg)
+    assert len(parts) > 0
+    for wave, trend, noise in parts:
+        logger.info('-------------------------------------------------')
+        logger.error('value,ma,wave,trend,noise\n' + '\n'.join(['{},{},{},{},{}'.format(value[i], avg[i], wave[i], trend[i], noise[i]) for i in range(len(wave))]))
+
+    assert False
